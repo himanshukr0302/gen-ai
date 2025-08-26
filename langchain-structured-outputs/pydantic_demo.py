8fcd7c29b3c169f1677981e9167223533ea31a60
himanshu@cachyos <student030205@gmail.com>
2025-08-26 13:31:56 +0530

    adding with_structured_output method use cases examples

---
+++ langchain-structured-outputs/pydantic_demo.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+
+class Student(BaseModel):
+    name: str = 'himanshu'
+    age: Optional[int] = None
+    grade: str = 'A'
+    email: EmailStr
+    cgpa: float = Field(gt=0,lt=10, default=5, description='A decimal value representing the cgpa of the student')
+
+
+new_student = {'age': '32', 'email': 'demo@gmail.com'}
+
+student = Student(**new_student)
+
+print(student)
---
+++ langchain-structured-outputs/with_structured_output_typeddict.py
@@ -0,0 +1,3 @@
+print("Gemini models do not support structured outputs like TypedDicts.")
+print("and the currently i don't  have the api keys for the supported ai's ")
+print("So i am leaving this file blank untill then")
